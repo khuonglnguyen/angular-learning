2c2c44e7822f0636ae5c329b4789a8cf2f6a4d6b
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-01 13:42:50 +0700

    10 Data Binding - *ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name = 'Khuong';
+  public age = 18;
   constructor() {}
 
   ngOnInit(): void {}
