03376a42b9dc22658d68930fee77ef191cccac7e
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-22 20:10:47 +0700

    16 - Casading Combobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,6 +19,28 @@ export class HomeComponent implements OnInit {
     },
   ];
 
+  public cities: any = [
+    {
+      city: '---Choose City---',
+    },
+    {
+      city: 'Hau Giang',
+      district: ['Long My', 'Vi Thanh'],
+    },
+    {
+      city: 'Can Tho',
+      district: ['Ninh Kieu', 'Binh Thuy'],
+    },
+  ];
+
+  public districts: any = [];
+
+  public selectedData(event: any) {
+    this.districts = this.cities.filter(
+      (item: any) => item.city === event.target.value
+    )[0].district;
+  }
+
   public resetName(): void {
     this.name = '';
   }
