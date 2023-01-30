b31630ccb9f12241e228407e8baf172b1088019b
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-30 20:53:36 +0700

    22 - Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestPipe } from './test.pipe';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AboutComponent } from './about/about.component';
 import { HighLightDirective } from './Directives/high-light.directive';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +21,14 @@ import { TemplateDrivenFormComponent } from './template-driven-form/template-dri
     PageNotFoundComponent,
     AboutComponent,
     HighLightDirective,
-    TemplateDrivenFormComponent
+    TemplateDrivenFormComponent,
+    ReactiveFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
